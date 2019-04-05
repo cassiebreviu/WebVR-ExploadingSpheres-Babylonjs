d751ec71e80aa5b7db0392ac28ff29f32bf6c3bb
cassieview <cassie.siljander@microsoft.com>
2019-04-04 19:23:31 -0500

    added material to sphere

--- src/sphere.ts
+++ src/sphere.ts
@@ -1,4 +1,5 @@
 import { Scene, Vector3, MeshBuilder, Mesh, ShadowGenerator, DirectionalLight, ActionManager, ExecuteCodeAction } from "babylonjs";
+import {StandardMaterial, Texture, Color3} from "babylonjs-materials";
 import { addParticlesToMesh, removeParticlesFromMesh } from "./particles";
 import { incrementScore } from "./score";
 
@@ -13,13 +14,20 @@ var addSphere = function (scene: Scene) {
     // Create sphere
     var sphereLight = new DirectionalLight("dir02", new Vector3(0.2, -1, 0), scene);
     sphereLight.position = new Vector3(0, 80, 0);
-    
+
     var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
     sphere.position = new Vector3(Math.random() * 20 - 10, 10, Math.random() * 10 - 5);
     sphere.material = new BABYLON.StandardMaterial("sphere material", scene)
     sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1 }, scene);
     var shadowGenerator = new ShadowGenerator(2048, sphereLight);
     shadowGenerator.addShadowCaster(sphere);
+
+
+    // Material
+    var materialAmiga = new StandardMaterial("amiga", scene);
+    materialAmiga.diffuseTexture = new Texture("textures/amiga.jpg", scene);
+    materialAmiga.emissiveColor = new Color3(0.5, 0.5, 0.5);
+    sphere.material = materialAmiga;
 
     sphere.actionManager = new ActionManager(scene);
     //add click event to sphere
